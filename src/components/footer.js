6469ac2f446921a08f8e7c2ad188aba4d2bfe9ca
sunecosuri <sunecosuri@gmail.com>
2019-07-14 08:34:49 +0900

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,9 +14,6 @@ const Footer = ({ copyrights }) => (
         <span className="footerCopyrights">
           © 2019 Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
         </span>
-        <span className="footerCopyrights">
-          Starter created by <a href="https://radoslawkoziel.pl">panr</a>
-        </span>
       </>
     )}
   </footer>
